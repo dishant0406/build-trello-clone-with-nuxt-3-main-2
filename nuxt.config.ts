a456485e0330e7c2619802e51efe2c471884c3e3
dishant <dishu5570@gmail.com>
2024-03-10 20:30:31 +0530

    Fix path to vello.css in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  css: ['/assets/css/vello.css'],
+  css: ['./assets/css/vello.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@pinia/nuxt', '@vueuse/nuxt'],
   colorMode: {
